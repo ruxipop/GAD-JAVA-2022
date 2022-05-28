1aaf04b697befee2e97e4ad263a7b7eefcd1934f
ruxipop <101018474+ruxipop@users.noreply.github.com>
2022-05-28 16:30:01 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.stream.IntStream;
-public class Main {
-    public static void main(String args[]){
-        IntStream.rangeClosed(1, 100)
-                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz," : "Fizz,") : (i % 5 == 0 ? "Buzz," : i+","))
-                .forEach(System.out::print);
-    }
-}
